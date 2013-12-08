4c02c6ed665f9c52e0be9b668cfc88caeb61f25f
mininet <mininet@mininet-vm.(none)>
2013-12-07 22:16:38 -0800

    Adding initia repo

---
+++ src/main/edu/columbia/cs6998/sdn/project/ServerInfo.java
@@ -0,0 +1,33 @@
+package main.edu.columbia.cs6998.sdn.project;
+
+public class ServerInfo {
+
+	
+		//private String ipAddress;
+		private double weight;
+		private double load;
+		
+		/*public String getIpAddress() {
+			return ipAddress;
+		}
+		public void setIpAddress(String ipAddress) {
+			this.ipAddress = ipAddress;
+		}*/
+		
+		public double getWeight() {
+			return weight;
+		}
+		public void setWeight(double weight) {
+			this.weight = weight;
+		}
+		public double getLoad() {
+			return load;
+		}
+		public void setLoad(double load) {
+			this.load = load;
+		}
+		
+		
+		
+		
+}
